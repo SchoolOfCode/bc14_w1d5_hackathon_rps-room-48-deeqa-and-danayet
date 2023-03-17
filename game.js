0b3279c7aba0340132197a41dd4113bb835dc990
DeeqaJamalini <127722546+DeeqaJamalini@users.noreply.github.com>
2023-03-17 14:38:00 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,29 +1,84 @@
-let userChoice; 
-let compChoice; 
+let userChoiceR = "rock";
+let userChoiceP = "paper";
+let userChoiceS = "scissor";
+
+
 let compChoiceR = "rock";
-let compChoiceS = "scissors";
-let compChoiceP = "paper"
+let compChoiceS = "scissor";
+let compChoiceP = "paper";
 
 
 
-function choices (choice1, choice2,){ 
-
-    compChoice = choice1;
-    userChoice = choie2; 
-   
-};
 
 
-if (userChoice && compChoice === "rock" || userChoice && compChoice === "scissors" ||  userChoice && compChoice === "paper" )
+let userChoice = userChoiceS;
+let compChoice = compChoiceP;
+
+
+
+if (userChoice === "rock" && compChoice === "scissor")
 {
-console.log("Try Again!")
+console.log("User won")
 }
 
 
  
-else if (userChoice === "rock" && compChoice === "paper" || userChoice ==="paper" && compChoice ==="scissors" || compChoice=== "rock" && userChoice === "scissors"){
+else if (userChoice === "rock" && compChoice === "paper"){
     
-    console.log("Computer Won!")
+    console.log("comp won")
 
 
+}
+
+else if (userChoice === "rock" && compChoice === "rock") 
+{
+ console.log("Try again")
+}
+
+
+
+
+
+
+else if (userChoice === "scissor" && compChoice === "scissor")
+{
+console.log("Try again")
+}
+
+
+ 
+else if (userChoice === "scissor" && compChoice === "paper"){
+    
+    console.log("User won")
+
+
+}
+
+else if (userChoice === "scissor" && compChoice === "rock") 
+{
+ console.log("computer won")
+}
+
+
+else if (userChoice === "paper" && compChoice === "scissor")
+{
+console.log("computer won")
+}
+
+
+ 
+else if (userChoice === "paper" && compChoice === "paper"){
+    
+    console.log("try again")
+
+}
+
+else if (userChoice === "paper" && compChoice === "rock") 
+{
+ console.log("User won")
 };
+
+
+
+
+
